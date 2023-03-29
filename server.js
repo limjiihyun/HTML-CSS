597b66814699386b4eac3f5a1fc1f611e736d746
limjiihyun <dlawlgus40@naver.com>
2023-03-29 23:37:12 +0900

    3/29

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true})) 
 
 app.listen(8080, function() {
     console.log('listening on 8080')
 })
 
 app.get('/', function(요청, 응답) { 
-  응답.sendFile(__dirname+'/start.html')
-});
-app.get('/index', function(요청, 응답) { 
-  응답.sendFile(__dirname+'/index.html')
-});
+  응답.sendFile(__dirname +'/start.html')
+})
+
+app.get('/index.html', function(요청, 응답) { 
+  응답.sendFile(__dirname +'/index.html')
+})
+
+app.get('/about.html', function(요청, 응답) { 
+  응답.sendFile(__dirname +'/about.html')
+})
+
+app.get('/project.html', function(요청, 응답) { 
+  응답.sendFile(__dirname +'/project.html')
+})
