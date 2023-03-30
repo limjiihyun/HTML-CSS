a48131ca2ff395c600d0c2b1296cb10d1ecb0961
limjiihyun <dlawlgus40@naver.com>
2023-03-31 00:43:36 +0900

    3/30

---
+++ router.js
@@ -0,0 +1,14 @@
+const { upload } = require("../image/smile.jpg");
+
+var app = express.Router();
+
+// 이미지 업로드
+app.post("/upload", upload.single("img"), async (req, res) => {
+
+  const imgfile = req.file;
+  console.log(imgfile);
+ 
+  
+}); //이미지 업로드 end
+
+module.exports = app;
